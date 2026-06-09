fd4e733f96ec4a644271453a6a9c1eb6c63ed5d2
Sebastián Sandoval <sebassantisandoval@gmail.com>
2026-06-08 21:06:12 -0300

    Update container.ts

--- launcher_app/src/container.ts
+++ launcher_app/src/container.ts
@@ -1,11 +1,11 @@
-// Composición de dependencias. Único lugar del proyecto que conoce tanto el dominio como la infraestructura concreta. Instancia y conecta adaptadores con casos de uso.
+/**Composición de dependencias.
+Único lugar del proyecto que conoce tanto el dominio como la infraestructura concreta.
+Instancia y conecta adaptadores con casos de uso.*/
+
 //Ej con caso de uso crearSolicitud:
-// src/container.ts
 import { CrearSolicitud } from "./modules/solicitudes/domain/use-cases/CrearSolicitud.usecase";
 import { PrismaSolicitudesRepository } from "./modules/solicitudes/infrastructure/adapters/PrismaSolicitudRepository";
-
 // 1. Instancia el adapter concreto (infraestructura)
 const solicitudesRepo = new PrismaSolicitudesRepository();
-
 // 2. Lo inyecta en el caso de uso (dominio)
 export const crearSolicitud = new CrearSolicitud(solicitudesRepo);
