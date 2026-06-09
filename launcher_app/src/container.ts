80ceb9815a5c675480201761f03d7741493971e1
Sebastián Sandoval <sebassantisandoval@gmail.com>
2026-06-09 14:34:39 -0300

    mod: se agrega adapter como export en container

--- launcher_app/src/container.ts
+++ launcher_app/src/container.ts
@@ -5,7 +5,11 @@ Instancia y conecta adaptadores con casos de uso.*/
 //Ej con caso de uso crearSolicitud:
 import { CrearSolicitud } from "./modules/solicitudes/domain/use-cases/CrearSolicitud.usecase";
 import { PrismaSolicitudesRepository } from "./modules/solicitudes/infrastructure/adapters/PrismaSolicitudRepository";
+import { ClerkAuthAdapter } from "./modules/auth/infrastructure/adapters/ClerkAuthAdapter";
 // 1. Instancia el adapter concreto (infraestructura)
 const solicitudesRepo = new PrismaSolicitudesRepository();
 // 2. Lo inyecta en el caso de uso (dominio)
 export const crearSolicitud = new CrearSolicitud(solicitudesRepo);
+
+//Auth
+export const authAdapter = new ClerkAuthAdapter();
--- launcher_app/src/modules/auth/infrastructure/adapters/ClerkAuthAdapter.ts
+++ launcher_app/src/modules/auth/infrastructure/adapters/ClerkAuthAdapter.ts
@@ -1,4 +1,4 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import {
   ForAuthenticating,
   UsuarioAutenticado,
@@ -8,15 +8,15 @@ export class ClerkAuthAdapter implements ForAuthenticating {
   async obtenerUsuarioActual(
     _req: Request,
   ): Promise<UsuarioAutenticado | null> {
-    const user = await currentUser();
-    if (!user) return null;
+    const { userId, sessionClaims } = await auth();
+    if (!userId) return null;
 
-    const rol = user.publicMetadata?.rol as UsuarioAutenticado["rol"];
+    const rol = sessionClaims?.metadata?.rol as UsuarioAutenticado["rol"];
     if (!rol) return null;
 
     return {
-      id: user.id,
-      email: user.emailAddresses[0].emailAddress,
+      id: userId,
+      email: sessionClaims?.email as string,
       rol,
     };
   }
