dd84d6474f005f7cd27e3738b9f6a75baeceffb5
Sebastián Sandoval <sebassantisandoval@gmail.com>
2026-06-10 19:04:48 -0300

    redireccion a login

--- launcher_app/src/middleware.ts
+++ launcher_app/src/middleware.ts
@@ -2,10 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
-  "/sign-up(.*)", // TODO: agregar rutas protegidas
+  "/sign-up(.*)", // TODO: agregar rutas no protegidas
+  "/auth/login",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
+  const { sessionClaims } = await auth(); // auth() devuelve el objeto completo
+  console.log("sessionClaims:", sessionClaims);
   if (!isPublicRoute(req)) {
     await auth.protect(); //redirigir a ...
   }
