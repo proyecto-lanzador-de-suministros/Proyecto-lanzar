7be11272393c418fc99e96bc1442be2349d26235
Sebastián Sandoval <sebassantisandoval@gmail.com>
2026-06-14 11:18:24 -0300

    def controlar soli (comentario)

---
+++ launcher_app/src/modules/solicitudes/domain/use-cases/ControlarSolicitud.usecase.ts
@@ -0,0 +1,3 @@
+/*El caso de uso para la transición a asignada es CU-09: Controlar solicitud. Está documentado en Documentos/cu-06-10.md:90:
+- creada → asignada si hay stock suficiente (asigna remitente automáticamente)
+- creada → rechazada si no hay stock*/
