063907377985091f9129d08ec8234a35f3d48826
Sebastián Sandoval <sebassantisandoval@gmail.com>
2026-06-17 11:26:30 -0300

    update (non-breaking change) db/prisma.client.ts

--- launcher_app/src/infrastructure/db/prisma.client.ts
+++ launcher_app/src/infrastructure/db/prisma.client.ts
@@ -4,12 +4,9 @@ import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaNeon(pool as any);
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
